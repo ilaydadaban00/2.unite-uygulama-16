c9c8c37ae42f03f47814315c421455b1a13e5bb6
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-03-06 01:10:08 +0300

    Add files via upload

---
+++ 2.unite uygulama  16/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.unite_uygulama__16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string secim = listBox1.SelectedItem.ToString();
+            bool cevap1 = radioButton1.Checked ;
+            bool cevap2 = radioButton2.Checked;
+            if(cevap1 == true &&(secim== "MP4" || secim == "MOV"))
+            {
+                MessageBox.Show("cevabınız dogru");
+            }
+             if (cevap2 == true && (secim == "JPG" || secim == "PNG"))
+            {
+                MessageBox.Show("cevabınız dogru");
+            }
+            if (cevap2 == false && (secim == "JPG" || secim == "PNG"))
+            {
+                MessageBox.Show("cevabınız YANLIŞTIR");
+            }
+            if (cevap1 == false && (secim == "MP4" || secim == "MOV"))
+            {
+                MessageBox.Show("cevabınız YANLIŞTIR");
+            }
+
+        }
+    }
+}
